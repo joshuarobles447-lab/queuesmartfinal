428502192393c4028159c5d304b6e194f40d3790
photobook <alexaoviedo184@gmail.com>
2026-05-04 14:13:16 +0800

    fix js routing in vercel

--- app.config.js
+++ app.config.js
@@ -3,8 +3,14 @@ dotenv.config();
 
 module.exports = ({ config }) => ({
   ...config,
+  name: "SmartQueue",
+  slug: "smartqueue",
+  web: {
+    bundler: 'metro',
+    output: 'static',
+  },
   extra: {
-    supabaseUrl: process.env.SUPABASE_URL,
-    supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+    supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL,
+    supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY,
   },
 });
